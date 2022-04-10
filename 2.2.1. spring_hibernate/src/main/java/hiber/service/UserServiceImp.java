96284e0c45da634a90cc1982f8c7d45fa472aeb5
Takhir Musakhanov <m.taha@mail.ru>
2022-04-10 11:35:22 +0400

    Не работает

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -1,12 +1,17 @@
 package hiber.service;
 
+import hiber.config.AppConfig;
 import hiber.dao.UserDao;
+import hiber.model.Car;
 import hiber.model.User;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.awt.*;
 import java.util.List;
+import java.util.Queue;
 
 @Service
 public class UserServiceImp implements UserService {
@@ -26,4 +31,17 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Transactional
+   @Override
+   public User getUserByCar(String model, int series) {
+      User user = null;
+      try(Session session = new AppConfig().getSessionFactory().getObject().openSession()) {
+         String HQL = "FROM User u LEFT OUTER JOIN FETCH u.car c WITH c.model=:m";
+         user = (User) session.createQuery(HQL).setParameter("m",model)
+                 .uniqueResult();
+      } catch (HeadlessException e) {
+         e.printStackTrace();
+      }
+      return user;
+   }
 }
